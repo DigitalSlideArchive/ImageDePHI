59ddf5d67dff11deb0fdd907cf869f601103d291
Brian Helba <brian.helba@kitware.com>
2022-12-09 15:50:36 -0500

    Add some type stubs for tifftools

---
+++ stubs/tifftools/__init__.pyi
@@ -0,0 +1,18 @@
+from .constants import Datatype, Tag, TiffDatatype, TiffTag
+from .exceptions import MustBeBigTiffError, TifftoolsError, UnknownTagError
+from .tifftools import read_tiff, write_tiff
+
+__version__: str
+
+__all__ = [
+    'Datatype',
+    'TiffDatatype',
+    'Tag',
+    'TiffTag',
+    'TifftoolsError',
+    'UnknownTagError',
+    'MustBeBigTiffError',
+    'read_tiff',
+    'write_tiff',
+    '__version__',
+]
