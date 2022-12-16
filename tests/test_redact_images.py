ee0aad5a0df7d82d1c1d2769415c8582696c29ae
naglepuff <mike.nagler@kitware.com>
2022-12-16 16:33:38 -0500

    Add E2E test, independent of tifftools

--- tests/test_redact_images.py
+++ tests/test_redact_images.py
@@ -1,41 +1,28 @@
 from pathlib import Path
 
-import tifftools
+from click.testing import CliRunner
+import pytest
 
-import imagedephi
-
-
-def verify_redaction_one_tag(ifd, tag, rule):
-    if rule['method'] == imagedephi.RedactMethod.REPLACE:
-        assert ifd['tags'][tag.value]['data'] == rule['replace_value']
-    elif rule['method'] == imagedephi.RedactMethod.DELETE:
-        assert tag.value not in ifd['tags']
+from imagedephi.__main__ import main
 
 
-def verify_redaction(ifds, rules):
-    for ifd in ifds:
-        for tag, tag_info in ifd['tags'].items():
-            tiff_tag = tifftools.constants.get_or_create_tag(
-                tag, tifftools.Tag, datatype=tifftools.Datatype[tag_info['datatype']]
-            )
-            if not tiff_tag.isIFD():
-                if tiff_tag.value in rules:
-                    verify_redaction_one_tag(ifd, tiff_tag, rules[tiff_tag.value])
-            else:
-                for sub_ifds in tag_info['ifds']:
-                    verify_redaction(sub_ifds, rules)
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner()
 
 
-def test_redact_images(tmp_path, mocker):
-    redact_one_image_spy = mocker.spy(imagedephi, 'redact_one_image')
-    input_dir = Path(__file__).parent / 'data' / 'input'
-    output_dir = tmp_path / 'output'
-    output_dir.mkdir()
-    imagedephi.redact_images(input_dir, output_dir)
+@pytest.fixture
+def data_dir() -> Path:
+    return Path(__file__).parent / "data"
 
-    assert redact_one_image_spy.call_count == 1
 
-    # ensure that each output file is properly redacted
-    for output_file in output_dir.iterdir():
-        tiff_info = tifftools.read_tiff(output_file)
-        verify_redaction(tiff_info['ifds'], imagedephi.get_tags_to_redact())
+def test_e2e(data_dir: Path, tmp_path: Path, runner: CliRunner) -> None:
+    result = runner.invoke(main, [str(data_dir / "input"), str(tmp_path)])
+
+    assert result.exit_code == 0
+    output_file = tmp_path / "REDACTED_test_image.tif"
+    assert output_file.exists()
+    with output_file.open("rb") as output_file_stream:
+        output_file_bytes = output_file_stream.read()
+        assert b"large_image_converter" not in output_file_bytes
+        assert b"Redacted by ImageDePHI" in output_file_bytes
