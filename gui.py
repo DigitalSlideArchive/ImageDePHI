b3615659f73da810d8341eb4ab1540c6535bef43
Mary Salvi <mary.salvi@kitware.com>
2022-12-19 15:33:11 -0500

    Move required packages to pyproject.toml
    format fixes
    
    Add .direnv to tox ignore

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from fastapi import FastAPI, Request, HTTPException, Form
+from fastapi import FastAPI, Form, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -9,24 +9,23 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-def select_directory(request: Request, path:str='/'):
+def select_directory(request: Request, path: str = "/"):
     base_path = pathlib.Path(path)
 
     if not base_path.exists():
         raise HTTPException(status_code=404, detail="Directory not found")
 
-    bread_crumbs = [
-        path for path in reversed(base_path.parents)
-    ]
+    bread_crumbs = [path for path in reversed(base_path.parents)]
     bread_crumbs.append(base_path)
 
-    directories = [
-        path_element for path_element in base_path.iterdir() if path_element.is_dir()
-    ]
+    directories = [path_element for path_element in base_path.iterdir() if path_element.is_dir()]
 
     return templates.TemplateResponse(
-        "DirectorySelector.html.j2", {"request": request, "directories": directories, "bread_crumbs": bread_crumbs}
+        "DirectorySelector.html.j2",
+        {"request": request, "directories": directories, "bread_crumbs": bread_crumbs},
     )
+
+
 @app.post("/directory_selection/")
-def selection(directory:str= Form()):
-    return { "message": "You chose this directory: %s" %directory }
+def selection(directory: str = Form()):  # noqa: B008
+    return {"message": "You chose this directory: %s" % directory}
