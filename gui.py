75f34ddd1c7aaed2b3c554a654ce2a341745789d
Mary Salvi <mary.salvi@kitware.com>
2022-12-19 15:03:37 -0500

    use full path for breadcrumbs and update naming

--- gui.py
+++ gui.py
@@ -12,16 +12,20 @@ templates = Jinja2Templates(directory="templates")
 def select_directory(request: Request, path:str='/'):
     base_path = pathlib.Path(path)
 
-    bread_crumbs = base_path.parts
     if not base_path.exists():
         raise HTTPException(status_code=404, detail="Directory not found")
 
-    directory_paths = [
+    bread_crumbs = [
+        path for path in reversed(base_path.parents)
+    ]
+    bread_crumbs.append(base_path)
+
+    directories = [
         path_element for path_element in base_path.iterdir() if path_element.is_dir()
     ]
 
     return templates.TemplateResponse(
-        "sample.html.j2", {"request": request, "directory_paths": directory_paths, "bread_crumbs": bread_crumbs}
+        "DirectorySelector.html.j2", {"request": request, "directories": directories, "bread_crumbs": bread_crumbs}
     )
 @app.post("/directory_selection/")
 def selection(directory:str= Form()):
