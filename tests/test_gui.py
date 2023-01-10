0d334653a3018ea5e714d4fd31762bdaf3489136
Mary Salvi <mary.salvi@kitware.com>
2023-01-10 12:42:37 -0500

    Update gui to fix bugs found in tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 from fastapi.testclient import TestClient
 import pytest
 
@@ -16,24 +17,40 @@ def test_gui_home(client: TestClient):
     assert response.status_code == 200
     assert "Select Directory" in response.text
 
-def test_gui_navigate_success(client: TestClient, tmp_path: Path,):
+
+def test_gui_navigate_success(
+    client: TestClient,
+    tmp_path: Path,
+):
     response = client.get("/?path=%s" % str(tmp_path))
 
     assert response.status_code == 200
 
-def test_gui_navigate_not_found(client: TestClient, tmp_path: Path,):
-    response = client.get("/?path=%s" % str(tmp_path/"fake") )
+
+def test_gui_navigate_not_found(
+    client: TestClient,
+    tmp_path: Path,
+):
+    response = client.get("/?path=%s" % str(tmp_path / "fake"))
 
     assert response.status_code == 404
     assert response.json() == {"detail": "Directory not found"}
 
-def test_gui_directory_selection(client: TestClient, tmp_path: Path,):
-    response = client.post("/directory_selection/", data={"directory":str(tmp_path)})
+
+def test_gui_directory_selection(
+    client: TestClient,
+    tmp_path: Path,
+):
+    response = client.post("/directory_selection/", data={"directory": str(tmp_path)})
 
     assert response.status_code == 200
     assert response.json() == {"message": "You chose this directory: %s" % tmp_path}
 
-def test_gui_directory_selection_failure(client: TestClient, tmp_path: Path,):
-    response = client.post("/directory_selection/", data={"directory":str(tmp_path/"fake")})
+
+def test_gui_directory_selection_failure(
+    client: TestClient,
+    tmp_path: Path,
+):
+    response = client.post("/directory_selection/", data={"directory": str(tmp_path / "fake")})
 
     assert response.status_code == 404
