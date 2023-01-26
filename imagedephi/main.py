ca4d389327f700dd4239c8a93bdc276ef0bc3b54
naglepuff <mike.nagler@kitware.com>
2023-01-26 17:16:41 -0500

    Pass base rules from root command
    Avoid duplicating logic of building the base ruleset across different commands.

--- imagedephi/main.py
+++ imagedephi/main.py
@@ -14,12 +14,19 @@ import yaml
 from imagedephi.async_utils import run_coroutine, wait_for_port
 from imagedephi.gui import app, shutdown_event
 from imagedephi.redact import build_ruleset, redact_images, show_redaction_plan
+from imagedephi.rules import RuleSet
+
+pass_base_ruleset = click.make_pass_decorator(RuleSet)
 
 
 @click.group
-def imagedephi() -> None:
+@click.pass_context
+def imagedephi(ctx) -> None:
     """Redact microscopy whole slide images."""
-    pass
+    base_rules_path = importlib.resources.files("imagedephi") / "base_rules.yaml"
+    with base_rules_path.open() as base_rules_stream:
+        base_rule_set = build_ruleset(yaml.safe_load(base_rules_stream))
+        ctx.obj = base_rule_set
 
 
 @imagedephi.command
@@ -36,29 +43,23 @@ def imagedephi() -> None:
     type=click.File("r"),
     help="Specify user-defined rules to override defaults",
 )
-def run(input_dir: Path, output_dir: Path, override_rules: TextIO | None):
+@pass_base_ruleset
+def run(base_rule_set: RuleSet, input_dir: Path, output_dir: Path, override_rules: TextIO | None):
     """Redact images in a folder according to given rule sets."""
-    base_rules_path = importlib.resources.files("imagedephi") / "base_rules.yaml"
-    with base_rules_path.open() as base_rules_stream:
-        base_rule_set = build_ruleset(yaml.safe_load(base_rules_stream))
-        override_rule_set = (
-            build_ruleset(yaml.safe_load(override_rules)) if override_rules else None
-        )
-        redact_images(input_dir, output_dir, base_rule_set, override_rule_set)
+    override_rule_set = build_ruleset(yaml.safe_load(override_rules)) if override_rules else None
+    redact_images(input_dir, output_dir, base_rule_set, override_rule_set)
 
 
 @imagedephi.command
 @click.argument("image", type=click.Path())
 @click.argument("override-rules", type=click.File("r"), required=False)
-def redaction_plan(image: click.Path, override_rules: TextIO | None) -> None:
+@pass_base_ruleset
+def redaction_plan(
+    base_rule_set: RuleSet, image: click.Path, override_rules: TextIO | None
+) -> None:
     """Print the redaction plan for a given image and rules."""
-    base_rules_path = importlib.resources.files("imagedephi") / "base_rules.yaml"
-    with base_rules_path.open() as base_rules_stream:
-        base_rule_set = build_ruleset(yaml.safe_load(base_rules_stream))
-        override_rule_set = (
-            build_ruleset(yaml.safe_load(override_rules)) if override_rules else None
-        )
-        show_redaction_plan(image, base_rule_set, override_rule_set)
+    override_rule_set = build_ruleset(yaml.safe_load(override_rules)) if override_rules else None
+    show_redaction_plan(image, base_rule_set, override_rule_set)
 
 
 @imagedephi.command
