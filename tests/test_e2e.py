361d59d963a257be25edc5b0bcf4abbe3c949d15
naglepuff <mike.nagler@kitware.com>
2023-01-26 15:01:12 -0500

    Update test to use option

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -32,10 +32,10 @@ def test_e2e_run(runner: CliRunner, data_dir: Path, tmp_path: Path) -> None:
             "run",
             str(data_dir / "input"),
             str(tmp_path),
+            "-r",
             str(data_dir / "rules" / "example_user_rules.yml"),
         ],
     )
-
     assert result.exit_code == 0
     output_file = tmp_path / "REDACTED_test_image.tif"
     assert output_file.exists()
