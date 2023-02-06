673bf77463731b16a1092eae04179009ffe1f1af
naglepuff <mike.nagler@kitware.com>
2023-02-06 16:46:02 -0500

    Create rule classes for SVS specific redaction

--- imagedephi/rules.py
+++ imagedephi/rules.py
@@ -108,6 +108,89 @@ class KeepMetadataTiffRule(MetadataTiffRule):
         pass
 
 
+class SvsDescription:
+    prefix: str
+    metadata: dict[str, str]
+
+    def __init__(self, svs_description_string: str):
+        description_components = svs_description_string.split("|")
+        self.prefix = description_components[0]
+        for metadata_component in description_components[1:]:
+            key, value = [token.strip() for token in metadata_component.split("=")]
+            self.metadata[key] = value
+
+    def __str__(self) -> str:
+        components = [self.prefix]
+        components = components + [
+            " = ".join([key, self.metadata[key]]) for key in self.metadata.keys()
+        ]
+        return "|".join(components)
+
+
+class SvsRule(Rule):
+    @abc.abstractmethod
+    def apply(self, svs_description: SvsDescription):
+        ...
+
+
+class MetadataSvsRule(SvsRule):
+    key: str
+    rule_type: RuleType
+
+    def __init__(self, rule_spec: dict, rule_source: RuleSource) -> None:
+        self.description = rule_spec.get("description", None)
+        self.redact_method = RedactMethod[rule_spec["method"].upper()]
+        self.rule_source = rule_source
+        self.key = rule_spec["key_name"]
+
+    def is_match(self, key: str) -> bool:
+        return self.key == key
+
+    @abs.abstractmethod
+    def apply(self, svs_description: SvsDescription):
+        ...
+
+    def get_description(self) -> str:
+        if self.description:
+            return self.description
+        return f"SVS Image Description - {self.key}: {self.redact_method} ({self.rule_source})"
+
+    @classmethod
+    def build(cls, rule_spec: dict, rule_source: RuleSource) -> MetadataSvsRule:
+        redact_method = RedactMethod[rule_spec["method"].upper()]
+        for rule_class in cls.__subclasses__():
+            if rule_class.redact_method == redact_method:
+                return rule_class(rule_spec, rule_source)
+            else:
+                raise Exception("Unknown redact_method.")
+
+
+class ReplaceMetadataSvsRule(MetadataSvsRule):
+    redact_method = RedactMethod.REPLACE
+    replace_value: str
+
+    def __init__(self, rule_spec: dict, source: RuleSource):
+        super().__init__(rule_spec, source)
+        self.replace_value = rule_spec["new_value"]
+
+    def apply(self, svs_description: SvsDescription):
+        svs_description.metadata[self.key] = self.replace_value
+
+
+class DeleteMetadataSvsRule(MetadataSvsRule):
+    redact_method = RedactMethod.DELETE
+
+    def apply(self, svs_description: SvsDescription):
+        del svs_description.metadata[self.key]
+
+
+class KeepMetadataSvsRule(MetadataSvsRule):
+    redact_method = RedactMethod.KEEP
+
+    def apply(self, svs_description: SvsDescription):
+        pass
+
+
 @dataclass
 class RuleSet:
     name: str
