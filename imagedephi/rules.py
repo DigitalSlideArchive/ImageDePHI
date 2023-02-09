662c734f988d906420237773598e8253fcf7ba30
naglepuff <mike.nagler@kitware.com>
2023-02-09 18:03:52 -0500

    Add SVS as a file format

--- imagedephi/rules.py
+++ imagedephi/rules.py
@@ -24,6 +24,7 @@ class RedactMethod(Enum):
 
 class FileFormat(Enum):
     TIFF = "tiff"
+    SVS = "svs"
 
 
 class RuleSource(Enum):
@@ -200,12 +201,18 @@ class RuleSet:
     def get_metadata_tiff_rules(self) -> list[MetadataTiffRule]:
         return [rule for rule in self.rules[FileFormat.TIFF] if isinstance(rule, MetadataTiffRule)]
 
+    def get_metadata_svs_rules(self) -> list[MetadataSvsRule]:
+        return [rule for rule in self.rules[FileFormat.SVS] if isinstance(rule, MetadataSvsRule)]
+
 
 def _build_rule(file_format: FileFormat, rule_spec: dict, rule_source: RuleSource) -> Rule:
     rule_type = RuleType[rule_spec["type"].upper()]
     if file_format == FileFormat.TIFF:
         if rule_type == RuleType.METADATA:
             return MetadataTiffRule.build(rule_spec, rule_source)
+    if file_format == FileFormat.SVS:
+        if rule_type == RuleType.METADATA:
+            return MetadataSvsRule.build(rule_spec, rule_source)
 
     raise Exception("Unsupported rule.")
 
