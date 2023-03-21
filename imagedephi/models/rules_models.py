222926df7486d6d51ee15bd409baf82461ec18d4
Mary Salvi <mary.salvi@kitware.com>
2023-03-21 08:47:23 -0400

    Refactor rules model.

--- imagedephi/models/rules_models.py
+++ imagedephi/models/rules_models.py
@@ -8,31 +8,67 @@ import yaml
 
 class Rule(BaseModel, abc.ABC):
     description: str | None = None
-    tag_name: str
-    method: Literal["replace", "keep", "delete"]
-    type: str
 
-class ReplaceRule(Rule):
+
+class MetadataRule(Rule):
+    type: Literal["metadata"]
+
+
+class TagMetadataRule(MetadataRule):
+    tag_name: str
+
+
+class ReplaceTagMetadataRule(TagMetadataRule):
     method: Literal["replace"]
     new_value: str
 
 
-class KeepRule(Rule):
+class KeepTagMetadataRule(TagMetadataRule):
     method: Literal["keep"]
 
 
-class DeleteRule(Rule):
+class DeleteTagMetadataRule(TagMetadataRule):
     method: Literal["delete"]
 
 
-# Pydantic needs to know which concrete rule types can actually be instantiated
-# and how to descriminate among them
-_ConcreteRule = Annotated[ReplaceRule | KeepRule | DeleteRule, Field(discriminator='method')]
+class KeyMetadataRule(MetadataRule):
+    key_name: str
+
+
+class ReplaceKeyMetadataRule(KeyMetadataRule):
+    method: Literal["replace"]
+    new_value: str
+
+
+class KeepKeyMetadataRule(KeyMetadataRule):
+    method: Literal["keep"]
+
+
+class DeleteKeyMetadataRule(KeyMetadataRule):
+    method: Literal["delete"]
+
+
+class Rules(BaseModel):
+    # Pydantic needs to know which concrete rule types can actually be instantiated
+    # and how to descriminate among them
+    tiff: list[
+        Annotated[
+            ReplaceTagMetadataRule | KeepTagMetadataRule | DeleteTagMetadataRule,
+            Field(discriminator="method"),
+        ]
+    ] | None
+    svs: list[
+        Annotated[
+            ReplaceKeyMetadataRule | KeepKeyMetadataRule | DeleteKeyMetadataRule,
+            Field(discriminator="method"),
+        ]
+    ] | None
+
 
 class RuleFile(BaseModel):
     name: str
     description: str
-    rules: dict[str, list[_ConcreteRule]]  # can add svs in future
+    rules: Rules
 
 
 base_rules_path = importlib.resources.files("imagedephi") / "base_rules.yaml"
@@ -42,6 +78,6 @@ with base_rules_path.open() as stream:
 
 print(rules_model)
 assert isinstance(rules_model, RuleFile)
-assert isinstance(rules_model.rules['tiff'][0], KeepRule)
-assert isinstance(rules_model.rules['tiff'][6], DeleteRule)
-assert issubclass(_ConcreteRule, Rule)
+assert rules_model.rules.tiff is not None
+assert isinstance(rules_model.rules.tiff[0], KeepTagMetadataRule)
+assert isinstance(rules_model.rules.tiff[6], DeleteTagMetadataRule)
