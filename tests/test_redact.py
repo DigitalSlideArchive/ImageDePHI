445f9d98f532bffa4ef43954553cb3bb76587342
naglepuff <mike.nagler@kitware.com>
2023-03-29 13:38:16 -0400

    Test different inputs

--- tests/test_redact.py
+++ tests/test_redact.py
@@ -12,7 +12,7 @@ def override_rule_set(data_dir):
         return build_ruleset(yaml.safe_load(rule_stream), RuleSource.OVERRIDE)
 
 
-def test_redact_svs(data_dir, tmp_path, override_rule_set):
+def test_redact_svs_dir(data_dir, tmp_path, override_rule_set):
     redact.redact_images(data_dir / "input" / "svs", tmp_path, override_rule_set)
 
     svs_output_file = tmp_path / "REDACTED_test_svs_image_blank.svs"
@@ -22,7 +22,27 @@ def test_redact_svs(data_dir, tmp_path, override_rule_set):
     assert b"ICC Profile" not in svs_output_file_bytes
 
 
-def test_plan_svs(capsys, data_dir, tmp_path, override_rule_set):
+def test_redact_svs_file(data_dir, tmp_path, override_rule_set):
+    redact.redact_images(
+        data_dir / "input" / "svs" / "test_svs_image_blank.svs", tmp_path, override_rule_set
+    )
+
+    svs_output_file = tmp_path / "REDACTED_test_svs_image_blank.svs"
+    assert svs_output_file.exists()
+    svs_output_file_bytes = svs_output_file.read_bytes()
+    # verify our custom svs rule was applied
+    assert b"ICC Profile" not in svs_output_file_bytes
+
+
+def test_plan_svs_dir(capsys, data_dir, tmp_path, override_rule_set):
+    redact.show_redaction_plan(data_dir / "input" / "svs", override_rule_set)
+    captured = capsys.readouterr()
+    assert "Replace ImageDescription" not in captured.out
+    assert "Aperio (.svs) Metadata Redaction Plan" in captured.out
+    assert "Delete ICC Profile" in captured.out
+
+
+def test_plan_svs_file(capsys, data_dir, tmp_path, override_rule_set):
     redact.show_redaction_plan(
         data_dir / "input" / "svs" / "test_svs_image_blank.svs", override_rule_set
     )
