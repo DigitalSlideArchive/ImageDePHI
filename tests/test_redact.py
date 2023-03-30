b771a4f9096e91fda21acc004db998cf4a8d9749
naglepuff <mike.nagler@kitware.com>
2023-03-30 13:39:42 -0400

    Use parametrized fixture

--- tests/test_redact.py
+++ tests/test_redact.py
@@ -1,3 +1,5 @@
+from pathlib import Path, PurePath
+
 import pytest
 import yaml
 
@@ -12,8 +14,16 @@ def override_rule_set(data_dir):
         return build_ruleset(yaml.safe_load(rule_stream), RuleSource.OVERRIDE)
 
 
-def test_redact_svs_dir(data_dir, tmp_path, override_rule_set):
-    redact.redact_images(data_dir / "input" / "svs", tmp_path, override_rule_set)
+@pytest.fixture(
+    params=[PurePath("svs"), PurePath("svs") / "test_svs_image_blank.svs"],
+    ids=["input_dir", "input_file"],
+)
+def svs_input_path(data_dir, request) -> Path:
+    return data_dir / "input" / request.param
+
+
+def test_redact_svs(svs_input_path, tmp_path, override_rule_set):
+    redact.redact_images(svs_input_path, tmp_path, override_rule_set)
 
     svs_output_file = tmp_path / "REDACTED_test_svs_image_blank.svs"
     assert svs_output_file.exists()
@@ -22,30 +32,9 @@ def test_redact_svs_dir(data_dir, tmp_path, override_rule_set):
     assert b"ICC Profile" not in svs_output_file_bytes
 
 
-def test_redact_svs_file(data_dir, tmp_path, override_rule_set):
-    redact.redact_images(
-        data_dir / "input" / "svs" / "test_svs_image_blank.svs", tmp_path, override_rule_set
-    )
+def test_plan_svs(capsys, svs_input_path, override_rule_set):
+    redact.show_redaction_plan(svs_input_path, override_rule_set)
 
-    svs_output_file = tmp_path / "REDACTED_test_svs_image_blank.svs"
-    assert svs_output_file.exists()
-    svs_output_file_bytes = svs_output_file.read_bytes()
-    # verify our custom svs rule was applied
-    assert b"ICC Profile" not in svs_output_file_bytes
-
-
-def test_plan_svs_dir(capsys, data_dir, tmp_path, override_rule_set):
-    redact.show_redaction_plan(data_dir / "input" / "svs", override_rule_set)
-    captured = capsys.readouterr()
-    assert "Replace ImageDescription" not in captured.out
-    assert "Aperio (.svs) Metadata Redaction Plan" in captured.out
-    assert "Delete ICC Profile" in captured.out
-
-
-def test_plan_svs_file(capsys, data_dir, tmp_path, override_rule_set):
-    redact.show_redaction_plan(
-        data_dir / "input" / "svs" / "test_svs_image_blank.svs", override_rule_set
-    )
     captured = capsys.readouterr()
     assert "Replace ImageDescription" not in captured.out
     assert "Aperio (.svs) Metadata Redaction Plan" in captured.out
