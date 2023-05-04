598fa5918120fda7bb96fd4e7b974938943b0551
naglepuff <mike.nagler@kitware.com>
2023-05-04 14:51:09 -0400

    Class for associated image redaction
    This commit contains an initial implementation of redaction of
    associated images for pure tiff files. It assumes the existence of
    a "default" associated image redaction rule, and applies that rule
    to all non-tiled images in the file.

--- imagedephi/redact/tiff.py
+++ imagedephi/redact/tiff.py
@@ -3,17 +3,26 @@ from __future__ import annotations
 from collections.abc import Generator
 from pathlib import Path
 from typing import TYPE_CHECKING
+import uuid
 
+from PIL import Image
 import tifftools
 import tifftools.constants
 
-from imagedephi.rules import ConcreteImageRule, ConcreteMetadataRule, FileFormat, TiffRules
+from imagedephi.rules import (
+    ConcreteImageRule,
+    ConcreteMetadataRule,
+    FileFormat,
+    ReplaceImageRule,
+    Ruleset,
+    TiffRules,
+)
 from imagedephi.utils.tiff import get_tiff_tag
 
 from .redaction_plan import RedactionPlan
 
 if TYPE_CHECKING:
-    from tifftools.tifftools import IFD, TiffInfo
+    from tifftools.tifftools import IFD, TagEntry, TiffInfo
 
 
 class TiffMetadataRedactionPlan(RedactionPlan):
@@ -129,3 +138,125 @@ class TiffImageRedactionPlan(RedactionPlan):
     file_format = FileFormat.TIFF
     tiff_info: TiffInfo
     redaction_steps: dict[int, ConcreteImageRule]
+    temp_dir: Path
+    image_dir: Path
+
+    def set_image_dir(self, image_dir: Path) -> None:
+        self.image_dir = image_dir
+
+    @staticmethod
+    def is_tiled(ifd: IFD):
+        """Determine if an IFD represents a tiled image."""
+        tile_width_tag = tifftools.constants.get_or_create_tag("TileWidth", tifftools.constants.Tag)
+        return tile_width_tag.value in ifd["tags"]
+
+    @staticmethod
+    def _iter_ifds(
+        ifds: list[IFD],
+        tag_set=tifftools.constants.Tag,
+    ) -> Generator[IFD, None, None]:
+        for ifd in ifds:
+            for tag_id, entry in ifd["tags"].items():
+                tag: tifftools.TiffTag = tifftools.constants.get_or_create_tag(
+                    tag_id,
+                    tagSet=tag_set,
+                    datatype=tifftools.Datatype[entry["datatype"]],
+                )
+                if not tag.isIFD():
+                    yield ifd
+                else:
+                    # entry['ifds'] contains a list of lists
+                    # see tifftools.read_tiff
+                    for sub_ifds in entry.get("ifds", []):
+                        yield from TiffImageRedactionPlan._iter_ifds(
+                            sub_ifds, tag.get("tagset")
+                        )
+            yield ifd
+
+    def get_associated_image_key_for_ifd(self, ifd: IFD):
+        """Use only the default rule if a more specific format can't be determined."""
+        return "default"
+
+    def __init__(
+        self, tiff_info: TiffInfo, temp_dir: Path, base_rule_set: Ruleset, override_rule_set: Ruleset | None = None
+    ) -> None:
+        self.tiff_info = tiff_info
+        self.temp_dir = temp_dir
+
+        self.redaction_steps = {}
+        override_rules = override_rule_set.tiff.associated_images if override_rule_set else {}
+        base_rules = base_rule_set.tiff.associated_images
+        merged_rules = base_rules | override_rules
+        ifds = self.tiff_info["ifds"]
+        for ifd in TiffImageRedactionPlan._iter_ifds(ifds):
+            if not TiffImageRedactionPlan.is_tiled(ifd):
+                associated_image_key = self.get_associated_image_key_for_ifd(ifd)
+                self.redaction_steps[ifd["offset"]] = merged_rules[associated_image_key]
+
+    def report_plan(self) -> None:
+        print("Tiff Associated Image Redaction Plan\n")
+        print(f"Found {len(self.redaction_steps)} associated images")
+        if (len(self.redaction_steps)):
+            default_rule = list(self.redaction_steps.values())[0]
+            print(f"Redaction action: {default_rule.action}")
+
+    def create_new_image(self, ifd: IFD, rule: ReplaceImageRule) -> Path:
+        image = None
+        if rule.replace_with == "black_square":
+            width = int(ifd["tags"][256]["data"][0])
+            length = int(ifd["tags"][257]["data"][0])
+            output_path = self.temp_dir / str(uuid.uuid4())
+            image = Image.new('RGB', (width, length))
+            image.save(output_path, "TIFF", compression="jpeg")
+            return output_path
+        raise Exception("Redaction option not currently supported")
+
+    def update_new_ifd(self, old_ifd: IFD, new_ifd: IFD) -> None:
+        for tag_value, entry in old_ifd["tags"].items():
+            if tag_value not in new_ifd["tags"].keys():
+                new_entry: TagEntry = {
+                    "datatype": entry["datatype"],
+                    "count": entry["count"],
+                    "data": entry["data"]
+                }
+                new_ifd["tags"][tag_value] = new_entry
+
+    def replace_associated_image(self, ifds: list[IFD], index: int, rule: ReplaceImageRule):
+        old_ifd = ifds[index]
+        new_image_path = self.create_new_image(old_ifd, rule)
+        replacement_tiff_info = tifftools.read_tiff(new_image_path)
+        new_ifd = replacement_tiff_info["ifds"][0]
+        self.update_new_ifd(old_ifd, new_ifd)
+        ifds[index] = new_ifd
+
+    def _execute_plan_inner(self, ifds: list[IFD], tag_set=tifftools.constants.Tag):
+        delete_ifd_indices = []
+        for index, ifd in enumerate(ifds):
+            for tag_id, entry in ifd["tags"].items():
+                tag = tifftools.constants.get_or_create_tag(
+                    tag_id,
+                    tagSet=tag_set,
+                    datatype=tifftools.Datatype[entry["datatype"]]
+                )
+                if tag.isIFD():
+                    for sub_ifds in entry.get("ifds", []):
+                        self._execute_plan_inner(sub_ifds, tag.get("tagset"))
+            if ifd["offset"] in self.redaction_steps:
+                rule = self.redaction_steps[ifd["offset"]]
+                if rule.action == "delete":
+                    delete_ifd_indices.append(index)
+                elif rule.action == "replace":
+                    self.replace_associated_image(ifds, index, rule)
+
+    def execute_plan(self) -> None:
+        ifds = self.tiff_info["ifds"]
+        self._execute_plan_inner(ifds)
+
+    def is_comprehensive(self) -> bool:
+        return True
+
+    def report_missing_rules(self) -> None:
+        print(
+            "The redaction plan is comprehensive. The default associaged image rule will be"
+            "applied to all associated images in the file."
+        )
