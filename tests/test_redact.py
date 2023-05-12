d9b93d0b7af7d404daf1409e56a7402b93f4ca17
Mary Salvi <mary.salvi@kitware.com>
2023-05-12 11:56:56 -0400

    Update tests to search for created directory

--- tests/test_redact.py
+++ tests/test_redact.py
@@ -1,5 +1,5 @@
-import datetime
 from pathlib import Path, PurePath
+import re
 
 import pytest
 import yaml
@@ -26,12 +26,12 @@ def svs_input_path(data_dir, request) -> Path:
 def test_redact_svs(svs_input_path, tmp_path, override_rule_set):
     redact.redact_images(svs_input_path, tmp_path, override_rule_set)
 
-    time_stamp = datetime.datetime.now().isoformat(timespec="seconds")
-    svs_output_file = tmp_path / f"Redacted_{time_stamp}/REDACTED_test_svs_image_blank.svs"
-    assert svs_output_file.exists()
-    svs_output_file_bytes = svs_output_file.read_bytes()
-    # verify our custom svs rule was applied
-    assert b"ICC Profile" not in svs_output_file_bytes
+    for x in tmp_path.iterdir():
+        if re.match(r"Redacted\_\d{4}\-\d{2}\-\d{2}[T]\d{2}\:\d{2}\:\d{2}", x.name):
+            for y in x.iterdir():
+                svs_output_file_bytes = y.read_bytes()
+                # verify our custom svs rule was applied
+                assert b"ICC Profile" not in svs_output_file_bytes
 
 
 def test_plan_svs(capsys, svs_input_path, override_rule_set):
