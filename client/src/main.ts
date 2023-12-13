2e56fef1ba1c9e0636858d0779babf130f5782a8
Mary Salvi <mary.salvi@kitware.com>
2023-12-13 14:02:24 -0500

    Add icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import { getDirectoryInfo } from './api/rest.ts'
+import 'remixicon/fonts/remixicon.css'
 
 createApp(App).mount('#app')
 getDirectoryInfo()
---
+++ client/src/store/directoryStore.ts
@@ -0,0 +1,6 @@
+import {ref} from 'vue'
+
+export  const selectedDirectories = ref({
+    inputDirectory: '/',
+    outputDirectory: '/'
+})
