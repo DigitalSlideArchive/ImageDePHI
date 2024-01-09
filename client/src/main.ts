52e60dcbd12a6a6fad143c4f7f89446bf27a726e
Mary Salvi <mary.salvi@kitware.com>
2024-01-09 09:22:53 -0500

    Update hatchling dependcies and web asset directory

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import { getDirectoryInfo } from './api/rest.ts'
 
 createApp(App).mount('#app')
+getDirectoryInfo()
