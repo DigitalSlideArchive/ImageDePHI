25f1b53242fdb2a1a0381ac5b37cc6f0dfebcc09
naglepuff <mike.nagler@kitware.com>
2024-03-11 17:32:38 -0400

    FIx file signature check for DICOM

--- imagedephi/utils/image.py
+++ imagedephi/utils/image.py
@@ -11,6 +11,6 @@ def get_file_format_from_path(image_path: Path) -> FileFormat | None:
     else:
         if data[:4] in (b"II\x2a\x00", b"MM\x00\x2a", b"II\x2b\x2a", b"MM\x00\x2b"):
             return FileFormat.TIFF
-        elif data[128:] == b"DCIM":
+        elif data[128:] == b"DICM":
             return FileFormat.DICOM
     return None
