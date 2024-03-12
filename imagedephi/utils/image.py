1e2d652a3c6d3ac05cdcd5c5f082bc3d3aca13be
naglepuff <mike.nagler@kitware.com>
2024-03-12 10:56:20 -0400

    Prefer dicom for dual-flavor images

--- imagedephi/utils/image.py
+++ imagedephi/utils/image.py
@@ -4,13 +4,16 @@ from imagedephi.rules import FileFormat
 
 
 def get_file_format_from_path(image_path: Path) -> FileFormat | None:
-    try:
-        data = open(image_path, "rb").read(132)
-    except PermissionError:
-        raise Exception(f"Could not open {image_path}")
-    else:
-        if data[:4] in (b"II\x2a\x00", b"MM\x00\x2a", b"II\x2b\x2a", b"MM\x00\x2b"):
-            return FileFormat.TIFF
-        elif data[128:] == b"DICM":
-            return FileFormat.DICOM
+    """
+    Attempt to determine the file type of an image by looking at its file signature.
+
+    See https://en.wikipedia.org/wiki/List_of_file_signatures. In case of a "dual-flavor" DICOM
+    file (i.e. a file that can be read as a DICOM or a tiff), prefer to report the image as
+    DICOM.
+    """
+    data = open(image_path, "rb").read(132)
+    if data[128:] == b"DICM":
+        return FileFormat.DICOM
+    elif data[:4] in (b"II\x2a\x00", b"MM\x00\x2a", b"II\x2b\x2a", b"MM\x00\x2b"):
+        return FileFormat.TIFF
     return None
