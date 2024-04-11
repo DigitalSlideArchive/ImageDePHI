18bb1c43fbf2b08eb041a48de5b7b56f28e5bc1e
Mary Salvi <mary.salvi@kitware.com>
2024-04-11 11:57:31 -0400

    Convert to yarn pnp

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import os from "os";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    fs:{
+      // Include the current yarn cache directory in the allow list
+      allow:['..',  `${os.homedir()}/.yarn/`]
+    }
+  }
 });
