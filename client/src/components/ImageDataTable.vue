7efab4d933e77c94a533598d286a99ae0ffa3fa0
Mary Salvi <mary.salvi@kitware.com>
2024-06-11 12:08:02 -0400

    Add delete action style and fix column alignment

--- client/src/components/ImageDataTable.vue
+++ client/src/components/ImageDataTable.vue
@@ -58,47 +58,52 @@ getThumbnail();
     >
       Loading.. <span class="loading loading-bars loading-md"></span>
     </div>
-    <table class="table table-auto table-xs w-full table-pin-rows table-pin-cols">
+    <table class="table table-auto w-full table-pin-rows table-pin-cols text-center">
     <thead>
-      <tr>
+      <tr class="text-base">
         <th class="bg-neutral text-white">Image</th>
-        <th v-if="Object.keys(imageRedactionPlan.data).includes('missing_tags')" class="bg-gray-600 text-white">Missing Tags</th>
-        <th v-for="tag in usedColumns" :key="tag" class="bg-gray-600 text-white">
+        <th v-if="Object.keys(imageRedactionPlan.data).includes('missing_tags')" class="bg-gray-600 text-white p-4">Missing Tags</th>
+        <th v-for="tag in usedColumns" :key="tag" class="bg-gray-600 text-white py-4 px-5">
           {{ tag }}
         </th>
       </tr>
     </thead>
-    <tbody>
+    <tbody class="text-base">
       <tr v-for="(image, index) in imageRedactionPlan.data" :key="index">
         <td>
           <img :src="image.thumbnail" class="w-20 h-20" />
         </td>
-        <td v-if="Object.keys(image).includes('missing_tags')">
+        <td>
+          <template v-if="Object.keys(image).includes('missing_tags')">
           <div
-            v-if="!image.missing_tags"
-            class="tooltip tooltip-right z-50 w-min"
-            data-tip="Ready to redact."
-          >
-            <i class="ri-checkbox-circle-fill text-green-600"></i>
-          </div>
-          <div
-            v-else-if="image.missing_tags.length > 0"
+            v-if="image.missing_tags.length > 0"
             class="tooltip tooltip-right z-50"
             :data-tip="`${image.missing_tags.length} tag(s) missing redaction rules.`"
           >
-            <i class="ri-error-warning-fill text-red-600 text-base"></i>
-            <td v-for="(obj, index) in image.missing_tags" :key="index">
+            <i class="ri-error-warning-fill text-red-600 text-xl"></i>
+            <div v-for="(obj, index) in image.missing_tags" :key="index">
             <span v-for="(value, key) in obj" :key="key">
               {{ key }}: {{ value }}
               </span>
-            </td>
+            </div>
+          </div>
+        </template>
+
+          <div
+            v-else
+            class="tooltip tooltip-right z-50"
+            :data-tip="`No missing redaction rules.`"
+          >
+            <i class="ri-checkbox-circle-fill text-green-600 text-xl"></i>
           </div>
         </td>
-        <td v-for="tag in usedColumns" :key="tag">
-          <span v-if="image[tag]" >
+        <template v-for="tag in usedColumns" :key="tag">
+        <td v-if="image[tag]">
+            <span  :class="image[tag].action === 'delete' ? 'line-through text-accent font-bold decoration-2' : ''">
           {{ image[tag].value}}
           </span>
         </td>
+      </template>
         </tr>
 
     </tbody>
