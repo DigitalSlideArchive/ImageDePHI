d5bc6eb764b8174701a8ee7a0d0d6e1d08c846b3
Mary Salvi <mary.salvi@kitware.com>
2024-06-11 12:05:46 -0400

    Add imageDescription rules for svs

--- client/src/store/rulesStore.ts
+++ client/src/store/rulesStore.ts
@@ -8,6 +8,7 @@ export async function fetchRulesData() {
   rules.value = [
     ...Object.keys(data.base_rules.tiff.metadata),
     ...Object.keys(data.base_rules.svs.metadata),
+    ...Object.keys(data.base_rules.svs.image_description),
     ...Object.keys(data.base_rules.dicom.metadata)
   ];
 }
