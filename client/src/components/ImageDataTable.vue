85507e74c7660b861a364e2ddc652546f09abab8
Mary Salvi <mary.salvi@kitware.com>
2024-07-02 10:01:00 -0400

    Add infinite scroll and placeholder image

--- client/src/components/ImageDataTable.vue
+++ client/src/components/ImageDataTable.vue
@@ -1,99 +1,108 @@
 <script setup lang="ts">
-
 defineProps({
   imageRedactionPlan: {
     type: Object,
     required: true,
   },
   usedColumns: {
-    type: Set,
-    required: true,
-  },
-  columnsComputed: {
-    type: Boolean,
+    type: Array<string>,
     required: true,
   },
 });
-
-
 </script>
 <template>
-    <div
-      v-if="!columnsComputed"
-      class="m-auto flex justify-center"
-    >
-      Loading.. <span class="loading loading-bars loading-md"></span>
-    </div>
-  <table v-if="columnsComputed" class="table table-auto text-center bg-base-100">
+  <div v-if="!usedColumns" class="m-auto flex justify-center">
+    Loading.. <span class="loading loading-bars loading-md"></span>
+  </div>
+  <table v-if="usedColumns" class="table table-auto text-center bg-base-100">
     <thead>
       <tr class="text-base">
         <th class="bg-neutral text-white py-5 px-6">Image File Name</th>
         <th class="bg-gray-600 text-white py-5 px-10">Image</th>
         <th class="bg-gray-600 text-white">Redaction Status</th>
-        <th v-if="Object.keys(imageRedactionPlan.data).includes('missing_tags')" class="bg-gray-600 text-white p-4">Missing Tags</th>
-        <th v-for="tag in usedColumns" :key="tag" class="bg-gray-600 text-white py-5 px-6">
+        <th
+          v-if="Object.keys(imageRedactionPlan.data).includes('missing_tags')"
+          class="bg-gray-600 text-white p-4"
+        >
+          Missing Tags
+        </th>
+        <th
+          v-for="tag in usedColumns"
+          :key="tag"
+          class="bg-gray-600 text-white py-5 px-6"
+        >
           {{ tag }}
         </th>
       </tr>
     </thead>
     <tbody class="text-base bg-base-100">
-      <tr v-for="(image, index) in imageRedactionPlan.data" :key="index">
-        <th> {{ index }}</th>
+      <tr
+        v-for="(image, index) in imageRedactionPlan.data"
+        :key="index"
+        class="h-[125px]"
+      >
+        <th>{{ index }}</th>
         <td>
           <img :src="image.thumbnail" class="w-20 h-20" />
         </td>
         <td>
-            <div
-              v-if="image.missing_tags"
-              class="tooltip tooltip-right"
-              :data-tip="`${image.missing_tags.length} tag(s) missing redaction rules.`"
-            >
-              <i class="ri-error-warning-fill text-red-600 text-xl"></i>
-              <div v-for="(obj, index) in image.missing_tags" :key="index">
+          <div
+            v-if="image.missing_tags"
+            class="tooltip tooltip-right"
+            :data-tip="`${image.missing_tags.length} tag(s) missing redaction rules.`"
+          >
+            <i class="ri-error-warning-fill text-red-600 text-xl"></i>
+            <div v-for="(obj, pos) in image.missing_tags" :key="pos">
               <span v-for="(value, key) in obj" :key="key">
                 {{ key }}: {{ value }}
-                </span>
-              </div>
+              </span>
             </div>
+          </div>
 
-            <div
-              v-else
-              class="tooltip tooltip-right"
-              :data-tip="`No missing redaction rules.`"
-            >
-              <i class="ri-checkbox-circle-fill text-green-600 text-xl"></i>
-            </div>
+          <div
+            v-else
+            class="tooltip tooltip-right"
+            :data-tip="`No missing redaction rules.`"
+          >
+            <i class="ri-checkbox-circle-fill text-green-600 text-xl"></i>
+          </div>
         </td>
         <template v-for="tag in usedColumns" :key="tag">
           <td class="text-ellipsis overflow-hidden max-w-[300px]">
-            <span v-if="image[tag]" :class="image[tag].action === 'delete' ? 'line-through text-accent font-bold decoration-2' : ''">
-              <span v-if="typeof image[tag].value == 'object'">
-                <span v-if="image[tag].value.length > 5">
-                {{Object.values(image[tag].value.slice(0, 5)).join(', ') + '...'}}
-                </span>
-                <span v-else>
-                {{ Object.values(image[tag].value).join(', ')}}
+            <span
+              v-if="image[tag]"
+              :class="
+                image[tag].action === 'delete'
+                  ? 'line-through text-accent font-bold decoration-2'
+                  : ''
+              "
+            >
+              <span v-if="image[tag].binary">
+                {{ image[tag].binary.bytes }} bytes
+                <span>
+                  {{ image[tag].binary.value.slice(0, 32) }}
                 </span>
               </span>
-              <span v-else class="text-ellipsis overflow-hidden">
-                {{ image[tag].value }}
-                </span>
-
+              <span v-else>
+                {{
+                  typeof image[tag].value === "object"
+                    ? image[tag].value.join(", ")
+                    : image[tag].value
+                }}
+              </span>
             </span>
-
           </td>
         </template>
-        </tr>
-
-      </tbody>
-    </table>
+      </tr>
+    </tbody>
+  </table>
 </template>
 <style scoped>
 thead th:first-child {
   position: sticky;
   left: 0;
   z-index: 2;
-  background-color: #201C35;
+  background-color: #201c35;
 }
 thead th {
   position: sticky;
@@ -103,7 +112,6 @@ thead th {
 
 tbody th {
   position: relative;
-
 }
 tbody th:first-child {
   position: sticky;
@@ -111,5 +119,4 @@ tbody th:first-child {
   z-index: 1;
   background-color: #ffffff;
 }
-
 </style>
--- client/src/components/InfiniteScroller.vue
+++ client/src/components/InfiniteScroller.vue
@@ -1,39 +1,38 @@
 <script setup lang="ts">
-import { ref, onMounted  } from "vue";
+import { ref, onMounted } from "vue";
 
-const emits = defineEmits(['infinite-scroll']);
+const emits = defineEmits(["infinite-scroll"]);
 
 const infiniteScroller = ref<HTMLTableElement | null>(null);
 const endOfTable = ref<HTMLDivElement | null>(null);
 
-
 onMounted(() => {
-  const observer = new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) {
-      emits('infinite-scroll');
-      console.log('Intersecting');
-    }
-
-  },
-  {
-    root: infiniteScroller.value,
-    rootMargin: '100px',
-
-
-  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        emits("infinite-scroll");
+      }
+    },
+    {
+      root: infiniteScroller.value,
+      rootMargin: "200px",
+    },
+  );
   observer.observe(endOfTable.value as Element);
 });
-
 </script>
 <template>
-<div ref="infiniteScroller" class="card rounded max-h-[90vh] max-w-[75vw] overflow-auto customScroll">
-  <slot></slot>
-  <div ref="endOfTable"></div>
-</div>
+  <div
+    ref="infiniteScroller"
+    class="card rounded max-h-[90vh] max-w-[75vw] overflow-auto customScroll"
+  >
+    <slot></slot>
+    <div ref="endOfTable"></div>
+  </div>
 </template>
 <style scoped>
 @supports selector(::-webkit-scrollbar) {
-  .customScroll::-webkit-scrollbar{
+  .customScroll::-webkit-scrollbar {
     width: 10px;
     height: 10px;
   }
@@ -49,11 +48,11 @@ onMounted(() => {
   }
 
   .customScroll::-webkit-scrollbar-thumb {
-    background-color: #00A6BF;
+    background-color: #00a6bf;
   }
 
   .customScroll::-webkit-scrollbar-track {
-    background-color: #9CA3AF;
+    background-color: #9ca3af;
     width: 5px;
   }
 }
