9b5e6feff4936ba1f063963126b23c5f14a6b42b
Mary Salvi <mary.salvi@kitware.com>
2024-09-23 12:49:20 -0400

    Fix infinite scroll bug

--- client/src/components/ImageDataDisplay.vue
+++ client/src/components/ImageDataDisplay.vue
@@ -4,6 +4,7 @@ import { useRedactionPlan } from "../store/imageStore";
 import { getRedactionPlan } from "../api/rest";
 import ImageDataTable from "./ImageDataTable.vue";
 import InfiniteScroller from "./InfiniteScroller.vue";
+import { selectedDirectories } from "../store/directoryStore";
 
 
 const limit = ref(50);
@@ -16,11 +17,12 @@ const loadImagePlan = async () => {
   ) {
     return;
   }
-  const newPlan = await getRedactionPlan(
-    useRedactionPlan.currentDirectory,
-    limit.value,
-    offset.value,
-    true,
+  const newPlan = await getRedactionPlan({
+    directory: useRedactionPlan.currentDirectory,
+    rules: selectedDirectories.value.rulesetDirectory,
+    limit: limit.value,
+    offset: offset.value,
+    update: true,}
   );
   useRedactionPlan.imageRedactionPlan.data = {
     ...useRedactionPlan.imageRedactionPlan.data,
