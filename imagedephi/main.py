8acd87e6771ee59b9e59c62cbaa896457e882da9
naglepuff <mike.nagler@kitware.com>
2024-11-19 14:47:28 -0500

    Create subcommand for getting demo data
    For now, just use the existing test image registry. A future commit will
    switch to using some mouse data.

--- imagedephi/main.py
+++ imagedephi/main.py
@@ -9,6 +9,9 @@ import webbrowser
 import click
 from hypercorn import Config
 from hypercorn.asyncio import serve
+import pooch
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 from imagedephi.gui.app import app
 from imagedephi.redact import ProfileChoice, redact_images, show_redaction_plan
@@ -227,3 +230,40 @@ async def gui(port: int) -> None:
                 shutdown_trigger=shutdown_event.wait,  # type: ignore
             )
         )
+
+
+@imagedephi.command
+@click.option(
+    "--data-dir",
+    type=click.Path(exists=True, file_okay=False, readable=True, writable=True, path_type=Path),
+    default=Path.cwd(),
+    help="Location where demo data will be downloaded.",
+)
+def demo_data(data_dir: Path):
+    """Download data for the Image DePHI demo to the specified directory."""
+    try:
+        demo_file_dir = data_dir / "demo_files"
+        demo_file_dir.mkdir(parents=True)
+    except PermissionError:
+        logger.error("Cannot create demo data directory, permission error")
+        raise
+    # TODO get these from a file
+    demo_files = {
+        "secret_metadata.tiff": "sha512:28be4880984bbb5f8e2a2e2314e594686427aa3da54e6a804ed5682e2da2585d21d3e3f751e995ad5b133a8b183cc7eb3cdee55cfcf5e4589d69c543db10fa9e",  # noqa: E501
+        "test_dcm_image.dcm": "sha512:4cb4c76799ce5d6f3f66c6d3bc055c5527b9fbef3d684450f48c870a5fbd882c87f3d59349ace778e80e78085276fecd278844af98a2324b754345aab6d6eac5",  # noqa: E501
+        "test_image.tif": "sha512:269d974a373f08e3274a1074fa595f8c70c496c64590412b6233eb2e3ce691c92963e5d0a3518848f15d9353783624faab18a002a9e049691cb5b627e40c9423",  # noqa: E501
+        "test_svs_image_blank.svs": "sha512:76350f39bafd86ced9d94b9a095200d3894034c36a3bc9a45a57a8f5ea820b8fd877ae483e9d9d4aef018cd8ca96c54ea815467ec94f6d692fca0ecc69afab23",  # noqa: E501
+    }
+    logger.info(f"Downloading files to {demo_file_dir}")
+    with logging_redirect_tqdm(loggers=[logger]):
+        for file_name in tqdm(
+            demo_files.keys(), desc="Downloading demo images...", position=0, leave=True
+        ):
+            hash = demo_files[file_name]
+            algo, hash_value = hash.split(":")
+            pooch.retrieve(
+                url=f"http://data.kitware.com/api/v1/file/hashsum/{algo}/{hash_value}/download",
+                known_hash=hash,
+                fname=file_name,
+                path=demo_file_dir,
+            )
